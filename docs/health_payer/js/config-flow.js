9eb44af40f2754ee0a05ab910c81593a43e66b73
Archana <agane@softserveinc.com>
2022-01-17 16:48:24 +0100

    Initial commit

---
+++ docs/health_payer/js/config-flow.js
@@ -0,0 +1,538 @@
+window.app = {
+  industry: 'health-payer',
+  herotext: {
+    title: 'hero_text',
+    titlespan: 'hero_span',
+    buttonlabel: 'learnmore',
+  },
+  secondarydetails: [
+    {
+      className: 'contact-us',
+      title: 'secondary1_title',
+      message: 'secondary1_msg',
+      link: 'contactus',
+    },
+    {
+      className: 'paperless',
+      title: 'secondary2_title',
+      message: 'secondary2_msg',
+      link: 'learnmore',
+    },
+  ],
+  primarydetails: [
+    {
+      img: 'img/option-1.jpg',
+      title: 'primary1_title',
+      message: 'primary1_msg',
+      link: 'learnmore',
+    },
+    {
+      img: 'img/option-2.jpg',
+      title: 'primary2_title',
+      message: 'primary2_msg',
+      link: 'learnmore',
+    },
+    {
+      img: 'img/option-3.jpg',
+      title: 'primary3_title',
+      message: 'primary3_msg',
+      link: 'learnmore',
+    },
+  ],
+  accountpicker: [
+    {
+      icon: 'pi-stethoscope',
+      title: 'accountpicker1_title',
+      desc: 'accountpicker1_desc',
+      id: '',
+    },
+    {
+      icon: 'pi-case-medical',
+      title: 'accountpicker2_title',
+      desc: 'accountpicker2_desc',
+      id: '',
+    },
+    {
+      icon: 'pi-shield',
+      title: 'accountpicker3_title',
+      desc: 'accountpicker3_desc',
+      id: '',
+    },
+  ],
+  recentactivity: [
+    {
+      message: 'recentactivity1_msg',
+    },
+    {
+      message: 'recentactivity2_msg',
+    },
+    {
+      message: 'recentactivity3_msg',
+    },
+    {
+      message: 'recentactivity4_msg',
+    },
+  ],
+  offers: [
+    {
+      img: 'offer-image.jpg',
+      title: 'offer1_title',
+      message: 'offer1_msg',
+    },
+  ],
+  menuitems: [
+    {
+      pagetype: 'individual',
+      title: 'menu1',
+    },
+    {
+      pagetype: 'employee',
+      title: 'menu2',
+    },
+    {
+      pagetype: 'provider',
+      title: 'menu3',
+    },
+    {
+      pagetype: 'agent',
+      title: 'menu4',
+    },
+  ],
+  landing: {
+    header: {
+      title: 'landing_header_title',
+      promotions: [
+        {
+          icon: 'pi-chevron-solid',
+          message: 'landing_header_promotion1',
+        },
+        {
+          icon: 'pi-chevron-solid',
+          message: 'landing_header_promotion2',
+        },
+        {
+          icon: 'pi-chevron-solid',
+          message: 'landing_header_promotion3',
+        },
+      ],
+    },
+    main_form: {
+      title: 'landing_main_form_title',
+      button_label: 'landing_main_form_button_label',
+      data: [
+        {
+          label: 'landing_main_form_data1_label',
+        },
+        {
+          label: 'landing_main_form_data2_label',
+        },
+        {
+          icon: 'pi-chevron-solid',
+          label: 'landing_main_form_data3_label',
+        },
+      ],
+    },
+    cards: {
+      title: 'landing_cards_title',
+      data: [
+        {
+          icon: 'pi-chevron-solid',
+          title: 'landing_cards_data1_title',
+          message: 'landing_cards_data1_message',
+        },
+        {
+          icon: 'pi-triangle-solid',
+          title: 'landing_cards_data2_title',
+          message: 'landing_cards_data2_message',
+        },
+        {
+          icon: 'pi-bolt-solid',
+          title: 'landing_cards_data3_title',
+          message: 'landing_cards_data3_message',
+        },
+      ],
+    },
+    details: [
+      {
+        title: 'landing_details1_title',
+        message: 'landing_details1_message',
+        image: 'landing-detail-1.png',
+      },
+      {
+        title: 'landing_details2_title',
+        message: 'landing_details2_message',
+        image: 'landing-detail-2.png',
+      },
+    ],
+    benefits: {
+      title: 'landing_benefits_title',
+      data: [
+        {
+          icon: 'pi-passbook',
+          message: 'landing_benefits_data1_message',
+        },
+        {
+          icon: 'pi-information-solid',
+          message: 'landing_benefits_data2_message',
+        },
+        {
+          icon: 'pi-receipt',
+          message: 'landing_benefits_data3_message',
+        },
+        {
+          icon: 'pi-bolt',
+          message: 'landing_benefits_data4_message',
+        },
+        {
+          icon: 'pi-disc-stack',
+          message: 'landing_benefits_data5_message',
+        },
+        {
+          icon: 'pi-bolt',
+          message: 'landing_benefits_data6_message',
+        },
+      ],
+    },
+    footer: {
+      title: 'landing_footer_title',
+      button_label: 'landing_footer_button_label',
+    },
+  },
+  offer: [{
+    header: {
+      title: 'offer_header_title',
+      msg: 'offer_header_msg',
+    },
+    main_offer: {
+      title: 'offer_main_offer_title',
+      message: 'offer_main_offer_msg',
+      button_label: 'offer_main_offer_button_label',
+      image: 'offer-main.jpg',
+    },
+    cards: {
+      title: 'offer_cards_title',
+      data: [
+        {
+          title: 'offer_cards_data1_title',
+          message: 'offer_cards_data1_msg',
+          highlight: 'offer_cards_data1_highlight',
+        },
+        {
+          title: 'offer_cards_data2_title',
+          message: 'offer_cards_data2_msg',
+          highlight: 'offer_cards_data2_highlight',
+        },
+        {
+          title: 'offer_cards_data3_title',
+          message: 'offer_cards_data3_msg',
+          highlight: 'offer_cards_data3_highlight',
+        },
+      ],
+    },
+    details: [
+      {
+        title: 'offer_details1_title',
+        message: 'offer_details1_msg',
+        image: 'offer-promo-1.jpg',
+      },
+    ],
+    highlights: {
+      data: [
+        {
+          icon: 'pi-heart-pulse',
+          title: 'offer_highlights_data1_title',
+          message: 'offer_highlights_data1_msg',
+        },
+        {
+          icon: 'pi-book-open',
+          title: 'offer_highlights_data2_title',
+          message: 'offer_highlights_data2_msg',
+        },
+        {
+          icon: 'pi-compass',
+          title: 'offer_highlights_data3_title',
+          message: 'offer_highlights_data3_msg',
+        },
+      ],
+    },
+    footer: {
+      title: 'offer_footer_title',
+      button_label: 'offer_footer_button_label',
+    },
+	// inserting new content as in retail_bank config FileUpload
+	},
+  {
+    header: {
+      title: 'offer1_header_title',
+      msg: 'offer1_header_msg',
+    },
+    main_offer: {
+      title: 'offer1_main_offer_title',
+      message: 'offer1_main_offer_msg',
+      button_label: 'offer1_main_offer_button_label',
+      image: 'offer-main.jpg',
+    },
+    cards: {
+      title: 'offer1_cards_title',
+      data: [
+        {
+          title: 'offer1_cards_data1_title',
+          message: 'offer1_cards_data1_msg',
+          highlight: 'offer1_cards_data1_highlight',
+        },
+        {
+          title: 'offer1_cards_data2_title',
+          message: 'offer1_cards_data2_msg',
+          highlight: 'offer1_cards_data2_highlight',
+        },
+        {
+          title: 'offer1_cards_data3_title',
+          message: 'offer1_cards_data3_msg',
+          highlight: 'offer1_cards_data3_highlight',
+        },
+      ],
+    },
+    details: [
+      {
+        title: 'offer1_details1_title',
+        message: 'offer1_details1_msg',
+        image: 'offer-promo-1.jpg',
+      },
+    ],
+    highlights: {
+      data: [
+        {
+          icon: 'pi-heart-pulse',
+          title: 'offer1_highlights_data1_title',
+          message: 'offer1_highlights_data1_msg',
+        },
+        {
+           icon: 'pi-book-open',
+          title: 'offer1_highlights_data2_title',
+          message: 'offer1_highlights_data2_msg',
+        },
+        {
+         icon: 'pi-compass',
+          title: 'offer1_highlights_data3_title',
+          message: 'offer1_highlights_data3_msg',
+        },
+      ],
+    },
+    footer: {
+      title: 'offer1_footer_title',
+      button_label: 'offer1_footer_button_label',
+    },
+  },
+  {
+    header: {
+      title: 'offer2_header_title',
+      msg: 'offer2_header_msg',
+    },
+    main_offer: {
+      title: 'offer2_main_offer_title',
+      message: 'offer2_main_offer_msg',
+      button_label: 'offer2_main_offer_button_label',
+      image: 'offer-main.jpg',
+    },
+    cards: {
+      title: 'offer2_cards_title',
+      data: [
+        {
+          title: 'offer2_cards_data1_title',
+          message: 'offer2_cards_data1_msg',
+          highlight: 'offer2_cards_data1_highlight',
+        },
+        {
+          title: 'offer2_cards_data2_title',
+          message: 'offer2_cards_data2_msg',
+          highlight: 'offer2_cards_data2_highlight',
+        },
+        {
+          title: 'offer2_cards_data3_title',
+          message: 'offer2_cards_data3_msg',
+          highlight: 'offer2_cards_data3_highlight',
+        },
+      ],
+    },
+    details: [
+      {
+        title: 'offer2_details1_title',
+        message: 'offer2_details1_msg',
+        image: 'offer-promo-1.jpg',
+      },
+    ],
+    highlights: {
+      data: [
+        {
+          icon: 'pi-heart-pulse',
+          title: 'offer2_highlights_data1_title',
+          message: 'offer2_highlights_data1_msg',
+        },
+        {
+          icon: 'pi-book-open',
+          title: 'offer2_highlights_data2_title',
+          message: 'offer2_highlights_data2_msg',
+        },
+        {
+          icon: 'pi-compass',
+          title: 'offer2_highlights_data3_title',
+          message: 'offer2_highlights_data3_msg',
+        },
+      ],
+    },
+    footer: {
+      title: 'offer_footer_title',
+      button_label: 'offer2_footer_button_label',
+    },
+  },
+  {
+    header: {
+      title: 'offer3_header_title',
+      msg: 'offer3_header_msg',
+    },
+    main_offer: {
+      title: 'offer3_main_offer_title',
+      message: 'offer3_main_offer_msg',
+      button_label: 'offer3_main_offer_button_label',
+      image: 'offer-main.jpg',
+    },
+    cards: {
+      title: 'offer3_cards_title',
+      data: [
+        {
+          title: 'offer3_cards_data1_title',
+          message: 'offer3_cards_data1_msg',
+          highlight: 'offer3_cards_data1_highlight',
+        },
+        {
+          title: 'offer3_cards_data2_title',
+          message: 'offer3_cards_data2_msg',
+          highlight: 'offer3_cards_data2_highlight',
+        },
+        {
+          title: 'offer3_cards_data3_title',
+          message: 'offer3_cards_data3_msg',
+          highlight: 'offer3_cards_data3_highlight',
+        },
+      ],
+    },
+    details: [
+      {
+        title: 'offer3_details1_title',
+        message: 'offer3_details1_msg',
+        image: 'offer-promo-1.jpg',
+      },
+    ],
+    highlights: {
+      data: [
+        {
+          icon: 'pi-heart-pulse',
+          title: 'offer3_highlights_data1_title',
+          message: 'offer3_highlights_data1_msg',
+        },
+        {
+          icon: 'pi-book-open',
+          title: 'offer3_highlights_data2_title',
+          message: 'offer3_highlights_data2_msg',
+        },
+        {
+          icon: 'pi-compass',
+          title: 'offer3_highlights_data3_title',
+          message: 'offer3_highlights_data3_msg',
+        },
+      ],
+    },
+    footer: {
+      title: 'offer3_footer_title',
+      button_label: 'offer3_footer_button_label',
+    },
+  },
+  {
+    header: {
+      title: 'offer4_header_title',
+      msg: 'offer4_header_msg',
+    },
+    main_offer: {
+      title: 'offer4_main_offer_title',
+      message: 'offer4_main_offer_msg',
+      button_label: 'offer4_main_offer_button_label',
+      image: 'offer-main.jpg',
+    },
+    cards: {
+      title: 'offer4_cards_title',
+      data: [
+        {
+          title: 'offer4_cards_data1_title',
+          message: 'offer4_cards_data1_msg',
+          highlight: 'offer4_cards_data1_highlight',
+        },
+        {
+          title: 'offer4_cards_data2_title',
+          message: 'offer4_cards_data2_msg',
+          highlight: 'offer4_cards_data2_highlight',
+        },
+        {
+          title: 'offer4_cards_data3_title',
+          message: 'offer4_cards_data3_msg',
+          highlight: 'offer4_cards_data3_highlight',
+        },
+      ],
+    },
+    details: [
+      {
+        title: 'offer4_details1_title',
+        message: 'offer4_details1_msg',
+        image: 'offer-promo-1.jpg',
+      },
+    ],
+    highlights: {
+      data: [
+        {
+          icon: 'pi-heart-pulse',
+          title: 'offer4_highlights_data1_title',
+          message: 'offer4_highlights_data1_msg',
+        },
+        {
+          icon: 'pi-book-open',
+          title: 'offer4_highlights_data2_title',
+          message: 'offer4_highlights_data2_msg',
+        },
+        {
+          icon: 'pi-compass',
+          title: 'offer4_highlights_data3_title',
+          message: 'offer4_highlights_data3_msg',
+        },
+      ],
+    },
+    footer: {
+      title: 'offer4_footer_title',
+      button_label: 'offer4_footer_button_label',
+    },
+	//end of new content from retail_bank config file
+  }],
+  phone: {
+    home_page: {
+      title: 'phone_home_page_title',
+      promo: {
+        image: 'option-2.jpg',
+      },
+      actions: [
+        {
+          icon: 'pi-calculator',
+          page: 'account',
+          title: 'phone_home_page_actions1_title',
+        },
+        {
+          icon: 'pi-document-data',
+          page: 'help',
+          title: 'phone_home_page_actions2_title',
+        },
+        {
+          icon: 'pi-dollar-sign',
+          page: 'heroaction',
+          title: 'phone_home_page_actions3_title',
+        },
+      ],
+    },
+  },
+};
---
+++ docs/precache-manifest.8c43c93fe7afc55064237e5a640521d1.js
@@ -0,0 +1,474 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": ".ee34a16a17c4f7ccbb94da77b76541",
+    "url": "./404.html"
+  },
+  {
+    "revision": ".2dc6aa4ffb448e731a5339b82cefdd",
+    "url": "./WEB-INF/web.xml"
+  },
+  {
+    "revision": ".c526b4b433071740f1fb534060e7bb",
+    "url": "./auto/css/override.css"
+  },
+  {
+    "revision": ".2e06e407ac80fd3f646bba3dd95ed5",
+    "url": "./auto/form-embed.html"
+  },
+  {
+    "revision": ".fa2c91649fdc992c36aa6c2b675cbe",
+    "url": "./auto/i18n/lang-en.js"
+  },
+  {
+    "revision": ".69d9db1419014cd82ec4f12b729d7a",
+    "url": "./auto/img/u+-logo.svg"
+  },
+  {
+    "revision": ".5c181f5c25f2982a749c4ba223f87c",
+    "url": "./auto/index.html"
+  },
+  {
+    "revision": ".86460ea51a5b1ba2dcd477e3a8ed70",
+    "url": "./auto/js/config-flow.js"
+  },
+  {
+    "revision": ".3374a4a8000d015aa50e07ac4dd6f5",
+    "url": "./auto/js/config-settings.js"
+  },
+  {
+    "revision": ".30fe7c553578a97695e84a310bbbdd",
+    "url": "./commercial_bank/charts/balance.html"
+  },
+  {
+    "revision": ".dd4cb0fb26045afb6b9f53a736c933",
+    "url": "./commercial_bank/charts/data.csv"
+  },
+  {
+    "revision": ".65eb5cab12a2a7bc4ea39472b0970e",
+    "url": "./commercial_bank/css/override.css"
+  },
+  {
+    "revision": ".2e06e407ac80fd3f646bba3dd95ed5",
+    "url": "./commercial_bank/form-embed.html"
+  },
+  {
+    "revision": ".1a2a6058f53844210e05d00a08aa6d",
+    "url": "./commercial_bank/i18n/lang-en.js"
+  },
+  {
+    "revision": ".d930e6e850492b8527d14ad46f26d1",
+    "url": "./commercial_bank/img/u+-logo.svg"
+  },
+  {
+    "revision": ".2985016b6fb437502e0a254d71b021",
+    "url": "./commercial_bank/index.html"
+  },
+  {
+    "revision": ".3dadd50a181cafa871cb2ca4781095",
+    "url": "./commercial_bank/js/config-flow.js"
+  },
+  {
+    "revision": ".404f18854a06b9387f4710d23f6acf",
+    "url": "./commercial_bank/js/config-settings.js"
+  },
+  {
+    "revision": ".2e2bb9512e8d97c83ff3ec4120938e",
+    "url": "./comms/css/override.css"
+  },
+  {
+    "revision": ".2e06e407ac80fd3f646bba3dd95ed5",
+    "url": "./comms/form-embed.html"
+  },
+  {
+    "revision": ".8caffd0783dc11bd2b9c7984e97f44",
+    "url": "./comms/i18n/lang-en.js"
+  },
+  {
+    "revision": ".1add469672c5799c3a2f0adbadf637",
+    "url": "./comms/img/u+-logo.svg"
+  },
+  {
+    "revision": ".e78e3aebd09dd876963c8d11cc9229",
+    "url": "./comms/index.html"
+  },
+  {
+    "revision": ".16cf31471f674abe4964ec20eecae0",
+    "url": "./comms/js/config-flow.js"
+  },
+  {
+    "revision": ".76ee4f3337cc6a0a5c37c7f00e3a22",
+    "url": "./comms/js/config-settings.js"
+  },
+  {
+    "revision": ".1e784feaf82f78009c",
+    "url": "./css/chunk-common.b6e72eca.css"
+  },
+  {
+    "revision": ".b994fd93882918b4f01909d8e20808",
+    "url": "./favicon.ico"
+  },
+  {
+    "revision": ".dbff858235c217e083cafb7922611c",
+    "url": "./fonts/uikit-icons.5fdbff85.woff"
+  },
+  {
+    "revision": ".8962bad68f9b57b6f66aba09bf526a",
+    "url": "./fonts/uikit-icons.f28962ba.woff2"
+  },
+  {
+    "revision": ".cf13cc7888b6e7730aa7e294befc8b",
+    "url": "./gov/css/override.css"
+  },
+  {
+    "revision": ".2e06e407ac80fd3f646bba3dd95ed5",
+    "url": "./gov/form-embed.html"
+  },
+  {
+    "revision": ".d6a6d7f35f3da410538b7f059df78a",
+    "url": "./gov/i18n/lang-en.js"
+  },
+  {
+    "revision": ".abe76efb5e9af39e16c89bdd4b0564",
+    "url": "./gov/img/u+-logo.svg"
+  },
+  {
+    "revision": ".18904625493c6e29edf9fb2d783930",
+    "url": "./gov/index.html"
+  },
+  {
+    "revision": ".c105ae7f64e5a325d6b7a21b489b3e",
+    "url": "./gov/js/config-flow.js"
+  },
+  {
+    "revision": ".c4c4d25b704d9b1e88b04523f152d7",
+    "url": "./gov/js/config-settings.js"
+  },
+  {
+    "revision": ".68c5cf29cd9630223e5f8083ea51d4",
+    "url": "./health_care/css/override.css"
+  },
+  {
+    "revision": ".2e06e407ac80fd3f646bba3dd95ed5",
+    "url": "./health_care/form-embed.html"
+  },
+  {
+    "revision": ".297143763e50063eedd78676989cef",
+    "url": "./health_care/i18n/lang-en.js"
+  },
+  {
+    "revision": ".bb413658287d9bcdb8789df76eaa40",
+    "url": "./health_care/img/u+-logo.svg"
+  },
+  {
+    "revision": ".13236f763465d1ccab0c3e6f9ab31a",
+    "url": "./health_care/index.html"
+  },
+  {
+    "revision": ".0a57e38d7325849174c901f02884b5",
+    "url": "./health_care/js/config-flow.js"
+  },
+  {
+    "revision": ".4ba578d04b3af53de25f79a82d929c",
+    "url": "./health_care/js/config-settings.js"
+  },
+  {
+    "revision": ".f39fa59cf7a73e77f8e9e5b746f9a0",
+    "url": "./health_payer/css/override.css"
+  },
+  {
+    "revision": ".2e06e407ac80fd3f646bba3dd95ed5",
+    "url": "./health_payer/form-embed.html"
+  },
+  {
+    "revision": ".9f57e82e8b70dd35daabe6f4e099b3",
+    "url": "./health_payer/i18n/lang-en.js"
+  },
+  {
+    "revision": ".bb413658287d9bcdb8789df76eaa40",
+    "url": "./health_payer/img/u+-logo.svg"
+  },
+  {
+    "revision": ".0510a1fb668742510e8a87f9e34b55",
+    "url": "./health_payer/index.html"
+  },
+  {
+    "revision": ".95e3f87b039b46fc383672a5b09bef",
+    "url": "./health_payer/js/config-flow.js"
+  },
+  {
+    "revision": ".ed0b34d604b90f7ff3134224a512b4",
+    "url": "./health_payer/js/config-settings.js"
+  },
+  {
+    "revision": ".68c5cf29cd9630223e5f8083ea51d4",
+    "url": "./health_pharma/css/override.css"
+  },
+  {
+    "revision": ".2e06e407ac80fd3f646bba3dd95ed5",
+    "url": "./health_pharma/form-embed.html"
+  },
+  {
+    "revision": ".eedc626e6274dc5d32683179065713",
+    "url": "./health_pharma/i18n/lang-en.js"
+  },
+  {
+    "revision": ".1fb448744330a692d78b9b4ce06265",
+    "url": "./health_pharma/img/u+-logo.svg"
+  },
+  {
+    "revision": ".f3882075ce05dae143979c59aa7eeb",
+    "url": "./health_pharma/index.html"
+  },
+  {
+    "revision": ".97d667afa78db0beb0debd9ff9ee86",
+    "url": "./health_pharma/js/config-flow.js"
+  },
+  {
+    "revision": ".73073b4b7c7f11fc3b421bde02f1ab",
+    "url": "./health_pharma/js/config-settings.js"
+  },
+  {
+    "revision": ".68c5cf29cd9630223e5f8083ea51d4",
+    "url": "./health_provider/css/override.css"
+  },
+  {
+    "revision": ".2e06e407ac80fd3f646bba3dd95ed5",
+    "url": "./health_provider/form-embed.html"
+  },
+  {
+    "revision": ".6b125aeb5ba7483dd3480433ff36cc",
+    "url": "./health_provider/i18n/lang-en.js"
+  },
+  {
+    "revision": ".bb413658287d9bcdb8789df76eaa40",
+    "url": "./health_provider/img/u+-logo.svg"
+  },
+  {
+    "revision": ".8179145d039aecd1a298839cf9d01a",
+    "url": "./health_provider/index.html"
+  },
+  {
+    "revision": ".2a9728a7979f64032de1f445f3af1a",
+    "url": "./health_provider/js/config-flow.js"
+  },
+  {
+    "revision": ".9041afa0b08f65b099c086c3efcf97",
+    "url": "./health_provider/js/config-settings.js"
+  },
+  {
+    "revision": ".63f620529651e8100b227cd232a945",
+    "url": "./img/ChatbotIcon.svg"
+  },
+  {
+    "revision": ".b3f3e21fae06fae9dd11b9f65b173d",
+    "url": "./img/checkmark-w.71b3f3e2.svg"
+  },
+  {
+    "revision": ".a3bcc30bfb7390bd168c6d644d46f2",
+    "url": "./img/contact.e5a3bcc3.svg"
+  },
+  {
+    "revision": ".c6b63072760f2685267ff8f787787d",
+    "url": "./img/paperless.3cc6b630.svg"
+  },
+  {
+    "revision": ".fa5d60dfdf08fb81f7634d97f233c9",
+    "url": "./index.html"
+  },
+  {
+    "revision": ".a3b2555329f029e9e7acb515c87847",
+    "url": "./insurance/css/override.css"
+  },
+  {
+    "revision": ".2e06e407ac80fd3f646bba3dd95ed5",
+    "url": "./insurance/form-embed.html"
+  },
+  {
+    "revision": ".c6af8b400ac4cf72b9d80fa1102ce8",
+    "url": "./insurance/i18n/lang-en.js"
+  },
+  {
+    "revision": ".13fcd55ed2a05023ba8ee73f0c45c5",
+    "url": "./insurance/img/u+-logo.svg"
+  },
+  {
+    "revision": ".67b96254fb7fb02fc10af75cfbfdc6",
+    "url": "./insurance/index.html"
+  },
+  {
+    "revision": ".5d82d38c91050c657130c6f87f2e5b",
+    "url": "./insurance/js/config-flow.js"
+  },
+  {
+    "revision": ".a355842830c3c8348d9e2290305aab",
+    "url": "./insurance/js/config-settings.js"
+  },
+  {
+    "revision": ".982994bdfccf369d9757a10f566db6",
+    "url": "./js/PegaHelper.js"
+  },
+  {
+    "revision": ".e03106aa104a8912bb983f83c261d8",
+    "url": "./js/PegaHelperExtension.js"
+  },
+  {
+    "revision": ".9a67dd743ae05dae3e",
+    "url": "./js/auto.57516cdc.js"
+  },
+  {
+    "revision": ".1e784feaf82f78009c",
+    "url": "./js/chunk-common.c5ef588b.js"
+  },
+  {
+    "revision": ".50bbca2e6c22bb6e2b",
+    "url": "./js/chunk-vendors.a1e369d5.js"
+  },
+  {
+    "revision": ".15ff06e344815610c9",
+    "url": "./js/commercial_bank.ee1653bd.js"
+  },
+  {
+    "revision": ".6d508240cc52964b2d",
+    "url": "./js/comms.ed27a957.js"
+  },
+  {
+    "revision": ".e359ddfb5658922d25",
+    "url": "./js/gov.8b91e644.js"
+  },
+  {
+    "revision": ".2b664d1f05c2577761",
+    "url": "./js/health_care.346a1a5d.js"
+  },
+  {
+    "revision": ".7d7c5e003cb56ac810",
+    "url": "./js/health_payer.585081ac.js"
+  },
+  {
+    "revision": ".235dad9a4192835e94",
+    "url": "./js/health_pharma.4c563891.js"
+  },
+  {
+    "revision": ".4b3202fc4d5389b166",
+    "url": "./js/health_provider.e3eb5ef4.js"
+  },
+  {
+    "revision": ".465e774ee4ba3d89c8",
+    "url": "./js/insurance.7f6e47ae.js"
+  },
+  {
+    "revision": ".0afd743d9e9643852e31a135a9f3ae",
+    "url": "./js/jquery-min.js"
+  },
+  {
+    "revision": ".563cf46945b577e450",
+    "url": "./js/manufacturing.dbe78e1c.js"
+  },
+  {
+    "revision": ".a6be5a0ed37a15bd2d14457ef3bb1e",
+    "url": "./js/pega-mashup-webcomponent-light-all.js"
+  },
+  {
+    "revision": ".dc03b468cb58a2782ffa2afe4aacb2",
+    "url": "./js/realtimecontainerscript.js"
+  },
+  {
+    "revision": ".ea4c080ffd6002243d",
+    "url": "./js/retail_bank.44b83018.js"
+  },
+  {
+    "revision": ".f63c67a32fc14479b0",
+    "url": "./js/wealth.183056bf.js"
+  },
+  {
+    "revision": ".29f346898fe1ec3ac3e485e6b63aed",
+    "url": "./manifest.json"
+  },
+  {
+    "revision": ".aefd97a1ad97229e670e461e7b1936",
+    "url": "./manufacturing/css/override.css"
+  },
+  {
+    "revision": ".2e06e407ac80fd3f646bba3dd95ed5",
+    "url": "./manufacturing/form-embed.html"
+  },
+  {
+    "revision": ".7a1acf9c64d1b54f432c651cf70ce4",
+    "url": "./manufacturing/i18n/lang-en.js"
+  },
+  {
+    "revision": ".8c11b784eb3c902a58900c6e68c0c5",
+    "url": "./manufacturing/img/u+-logo.svg"
+  },
+  {
+    "revision": ".0ffefd974a25236014e744da2fca9b",
+    "url": "./manufacturing/index.html"
+  },
+  {
+    "revision": ".ee83510a91ce84d83ddf45f30e34f5",
+    "url": "./manufacturing/js/config-flow.js"
+  },
+  {
+    "revision": ".ce3785106c6cd61a5d1edc08bbb429",
+    "url": "./manufacturing/js/config-settings.js"
+  },
+  {
+    "revision": ".c61a67245a0965b83daf1a70ae1e08",
+    "url": "./retail_bank/css/override.css"
+  },
+  {
+    "revision": ".2e06e407ac80fd3f646bba3dd95ed5",
+    "url": "./retail_bank/form-embed.html"
+  },
+  {
+    "revision": ".7efba2f9692ba36e0009ce406b70b3",
+    "url": "./retail_bank/i18n/lang-en.js"
+  },
+  {
+    "revision": ".32d0e0a8269b4e2257e5dba0a79899",
+    "url": "./retail_bank/i18n/lang-fr.js"
+  },
+  {
+    "revision": ".d930e6e850492b8527d14ad46f26d1",
+    "url": "./retail_bank/img/u+-logo.svg"
+  },
+  {
+    "revision": ".86c1404ca4ea7724b503fe586f2368",
+    "url": "./retail_bank/index.html"
+  },
+  {
+    "revision": ".c3aef9c894f5a822ec26299013d1c3",
+    "url": "./retail_bank/js/config-flow.js"
+  },
+  {
+    "revision": ".9af4cd59892562023a29cab499ef20",
+    "url": "./retail_bank/js/config-settings.js"
+  },
+  {
+    "revision": ".c61a67245a0965b83daf1a70ae1e08",
+    "url": "./wealth/css/override.css"
+  },
+  {
+    "revision": ".2e06e407ac80fd3f646bba3dd95ed5",
+    "url": "./wealth/form-embed.html"
+  },
+  {
+    "revision": ".321c9d2cf3cad83b34bd2c21af52ad",
+    "url": "./wealth/i18n/lang-en.js"
+  },
+  {
+    "revision": ".732fa62f1d833c361159cbae1f1c84",
+    "url": "./wealth/img/u+-logo.svg"
+  },
+  {
+    "revision": ".7553b55af5dee31c13c3ffd4c31c2d",
+    "url": "./wealth/index.html"
+  },
+  {
+    "revision": ".f424f8c55e31b2a68a1721c619eada",
+    "url": "./wealth/js/config-flow.js"
+  },
+  {
+    "revision": ".81eafa9d5cba9a3ba2392b57fb06d6",
+    "url": "./wealth/js/config-settings.js"
+  }
+]);
